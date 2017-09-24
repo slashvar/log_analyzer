2e27736f8743f8b0437c2e1018eec1d25dc67536
Marwan Burelle <marwan@lse.epita.fr>
2017-09-25 01:10:06 +0200

    Add source code files

---
+++ log_analyzer.cc
@@ -0,0 +1,231 @@
+/*
+ * log_analyzer.cc : entry point and cmd line parsing
+ * cmd line and cmd parsing are volontary kept simple.
+ *
+ * The program accept a file name as input and can read query on stdin working
+ * on the same file for each query.
+ *
+ * You can also pass a single query on the command line.
+ */
+
+#include <algorithm>
+#include <array>
+#include <cstdlib>
+#include <fstream>
+#include <iostream>
+#include <utility>
+#include <vector>
+
+#ifndef NO_ISATTY
+#include <unistd.h>
+#endif /* NO_ISATTY */
+
+#include "reader.hh"
+#include "time_tree.hh"
+
+/*
+ * query operations helper functions
+ */
+
+static
+void top_freq(time_tree<std::string>& tree, date start, date stop, size_t n)
+{
+  using hdata = std::pair<std::string, size_t>;
+  static auto cmp = [] (const hdata& p1, const hdata& p2) -> bool {
+		      return p1.second < p2.second;
+		    };
+
+  auto hist = tree.compute_freq(start, stop);
+
+  std::vector<hdata> data;
+  for (auto p : hist)
+    data.push_back(p);
+  std::make_heap(begin(data), end(data), cmp);
+
+  for (size_t i = 0; i < std::min(n, data.size()); i++) {
+    std::pop_heap(begin(data), end(data), cmp);
+    auto p = data.back();
+    data.pop_back();
+    std::cout << p.first << "\t" << p.second << std::endl;
+  }
+}
+
+static
+void count(time_tree<std::string>& tree, date begin, date end)
+{
+  std::cout << tree.count(begin, end) << std::endl;
+}
+
+/*
+ * query forms:
+ *
+ * In both forms, elements must be separated by space chars on a single line.
+ *
+ * count begin end
+ * counts the number of query between begin and end
+ *
+ * top begin end nb
+ * display the nb most frequent query between begin and end
+ *
+ * begin and end have the same format as the input dates: yyyy-mm-dd hh:min:sec
+ *
+ * *TODO*:
+ *	* support partial date format
+ *	* better command parsing
+ */
+
+static
+bool parse_query(time_tree<std::string>& tree, const char *line)
+{
+  int n, pos;
+  char kwd[6];
+  n = std::sscanf(line, "%5s%n", kwd, &pos);
+  if (n < 1)
+    return false;
+  if (std::string("count") == kwd) {
+    std::array<unsigned, 6> dbegin, dend;
+    int oldpos = pos;
+    if (!read_date(line + pos, dbegin, pos)) {
+      return false;
+    }
+    pos += oldpos;
+    if (!read_date(line + pos, dend, pos)) {
+      return false;
+    }
+    count(tree, date(dbegin), date(dend));
+    return true;
+  }
+  if (std::string("top") == kwd) {
+    std::array<unsigned, 6> dbegin, dend;
+    int oldpos = pos;
+    if (!read_date(line + pos, dbegin, pos))
+      return false;
+    pos += oldpos;
+    oldpos = pos;
+    if (!read_date(line + pos, dend, pos))
+      return false;
+    pos += oldpos;
+    size_t nth;
+    char *stop;
+    nth = std::strtoul(line + pos, &stop, 10);
+    if (line + pos == stop || nth == 0)
+      return false;
+    top_freq(tree, date(dbegin), date(dend), nth);
+    return true;
+  }
+  return false;
+}
+
+static
+size_t read_data(std::istream& in, time_tree<std::string>& tree)
+{
+  auto consume = [&tree] (std::array<unsigned, 6> d, std::string cont) {
+    tree.insert(date(d), cont);
+  };
+
+  auto count = read_log(in, std::clog, consume);
+  return count;
+}
+
+static
+time_tree<std::string> read_data_from_file(const char* fname)
+{
+  std::ifstream infile(fname);
+  if (!infile) {
+    std::clog << "Can't open file " << fname << std::endl;
+    std::exit(1);
+  }
+
+  time_tree<std::string> tree;
+  auto count = read_data(infile, tree);
+
+  if (count == 0) {
+    std::clog << "Empty content in input file " << fname << std::endl;
+    std::exit(1);
+  }
+
+  return tree;
+}
+
+static
+time_tree<std::string> read_data_from_stdin(void)
+{
+  time_tree<std::string> tree;
+  auto count = read_data(std::cin, tree);
+
+  if (count == 0) {
+    std::clog << "Empty content in input\n";
+    std::exit(1);
+  }
+
+  return tree;
+}
+
+static
+void usage(const char *cmdname)
+{
+  std::clog << cmdname << " [-e query] [filename]\n";
+  std::clog << "  -e query\tperform a single query\n\n";
+
+  std::clog << "if [filename] is \"-\" use stdin as input, in that case \"-e\" option is mandatory.\n";
+
+}
+
+static
+void prompt(void)
+{
+# ifndef NO_ISATTY
+  if (isatty(STDIN_FILENO))
+# endif
+    std::cout << "> ";
+}
+
+int main(int argc, char *argv[])
+{
+  if (argc < 2) {
+    std::clog << "Missing file name or option\n";
+    usage(argv[0]);
+    return 1;
+  }
+  bool single = false;
+  int pos = 1;
+  if (std::string("-e") == argv[1]) {
+    single = true;
+    if (argc < 3) {
+      std::clog << "Missing argument after -e\n";
+      usage(argv[0]);
+      return 1;
+    }
+    pos += 2;
+  }
+
+  bool usestdin = false;
+
+  if (argc < pos + 1 || std::string("-") == argv[pos]) {
+    if (!single) {
+      std::clog << "Usage of -e option is mandatory with stdin input.\n";
+      return 1;
+    }
+    usestdin = true;
+  }
+
+  auto tree = usestdin ? read_data_from_stdin() : read_data_from_file(argv[pos]);
+
+  if (single) {
+    if (!parse_query(tree, argv[2])) {
+      std::clog << "Query parse error.\n";
+      return 1;
+    }
+  } else {
+    char line[256];
+    prompt();
+    while (std::cin.getline(line, 256)) {
+      if (!parse_query(tree, line)) {
+	std::clog << "Query parse error.\n";
+      }
+      prompt();
+    }
+  }
+
+  return 0;
+}
---
+++ time_tree.hh
@@ -0,0 +1,134 @@
+/*
+ * time_tree.hh: time oriented tree
+ * some hybrid construction between prefix tree and B+ tree
+ * First level is dispatched against year, then month, day, hour, minute and
+ * finally seconds. Last level (seconds) contains a containers.
+ * Last level is fully linked to avoid traversal for range query.
+ */
+
+#ifndef _ALGOLIA_INTERVIEW_TIME_TREE_HH_
+#define _ALGOLIA_INTERVIEW_TIME_TREE_HH_
+
+#include <algorithm>
+#include <array>
+#include <cassert>
+#include <memory>
+#include <unordered_map>
+#include <vector>
+
+struct date {
+  date(const std::array<unsigned, 6>& s) : split(s) {
+    value = 0;
+    for (unsigned x : split) {
+      value = value * 100 + x;
+    }
+  }
+  std::uint64_t value;
+  std::array<unsigned, 6> split;
+};
+
+template <typename CONTENT>
+struct time_tree {
+  struct node {
+    size_t rank(date d) {
+      auto cmp = [](const date d1, const date d2) { return d1.value < d2.value; };
+      auto target = std::lower_bound(begin(keys), end(keys), d, cmp);
+      return std::distance(begin(keys), target);
+    }
+    std::vector<date> keys;
+    std::vector<std::shared_ptr<node>> children;
+    std::weak_ptr<node> next, prev;
+    std::vector<CONTENT> store;
+  };
+
+  time_tree() : root(std::make_shared<node>()) {
+    date ld {{{0, 0, 0, 0, 0, 0}}};
+    date rd {{{9999, 12, 31, 23, 59, 59}}};
+    auto cur = root;
+    for (int i = 0; i < 5; i++) {
+      cur->keys.push_back(ld);
+      cur->children.push_back(std::make_shared<node>());
+      cur = cur->children.back();
+    }
+    cur->keys.push_back(ld);
+    left_sentinel = cur;
+    cur = root;
+    for (int i = 0; i < 5; i++) {
+      cur->keys.push_back(rd);
+      cur->children.push_back(std::make_shared<node>());
+      cur = cur->children.back();
+    }
+    cur->keys.push_back(rd);
+    right_sentinel = cur;
+    left_sentinel.lock()->next = cur;
+    cur->prev = left_sentinel.lock();
+  }
+
+  std::shared_ptr<node> _upper(const date& target, std::shared_ptr<node> cur) {
+    for (; cur->children.size() > 0; cur = cur->children[std::min(cur->rank(target), cur->children.size() - 1)])
+      continue;
+    return cur;
+  }
+
+  std::shared_ptr<node> upper_bound(const date& target)
+  {
+    auto cur = _upper(target, root);
+    return cur;
+  }
+
+  void insert(const date& d, CONTENT x) {
+    auto cur = root;
+    for (unsigned i = 0; i < 6; i++ ) {
+      size_t rk = cur->rank(d);
+      if (rk > 0 && rk >= cur->keys.size() && cur->keys[rk-1].split[i] == d.split[i])
+        rk -= 1;
+      if (rk >= cur->keys.size() || cur->keys[rk].split[i] != d.split[i]) {
+        auto prev = _upper(d, cur);
+        if (prev->prev.lock())
+          prev = prev->prev.lock();
+        for (unsigned j = i; j < 6; j++) {
+          cur->keys.insert(begin(cur->keys) + rk, d);
+          cur = *cur->children.insert(begin(cur->children) + rk, std::make_shared<node>());
+          rk = 0;
+        }
+        cur->keys.push_back(d);
+        cur->store.push_back(x);
+        cur->prev = prev;
+        cur->next = prev->next.lock();
+        prev->next = cur;
+        if (cur->next.lock())
+          cur->next.lock()->prev = cur;
+        return;
+      }
+      cur = cur->children[std::min(rk, cur->children.size() - 1)];
+    }
+    cur->store.push_back(x);
+  }
+
+  size_t count(const date& start, const date& end_) {
+    auto begin = upper_bound(start);
+    auto end = upper_bound(end_);
+    size_t count = 0;
+    for (auto cur = begin; cur && cur != end; cur = cur->next.lock()) {
+      count += cur->store.size();
+    }
+    return count;
+  }
+
+  std::unordered_map<std::string, size_t> compute_freq(const date& start, const date& end_) {
+    auto begin = upper_bound(start);
+    auto end = upper_bound(end_);
+    std::unordered_map<std::string, size_t> hist;
+    for (auto cur = begin; cur && cur != end; cur = cur->next.lock()) {
+      for (auto q : cur->store)
+        hist[q] += 1;
+    }
+    return hist;
+  }
+
+  std::shared_ptr<node> root;
+  std::weak_ptr<node> left_sentinel, right_sentinel;
+
+};
+
+#endif
